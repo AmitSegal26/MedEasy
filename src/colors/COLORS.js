669c45b9ff97cd6f0c00edc011f5901523956791
Amit <amitseg2612@gmail.com>
2023-09-17 20:24:57 +0300

    color palette update

--- src/colors/COLORS.js
+++ src/colors/COLORS.js
@@ -1,8 +1,8 @@
 const COLORS = {
   MAIN: "#E0B4A4",
   SECONDARY: "#B2BCBB",
-  TEXT1: "#22404A",
-  TEXT2: "#4C6A78",
+  TEXT1: "#0f0f0f",
+  TEXT2: "#1f1f1f",
   BACKGROUND: "#E4DCCF",
   INVERTEDFROMMAIN: "#1f4b5b",
 };
